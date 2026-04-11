5a511900a03f3b81f560fb424f4daeab7d754be9
treeses757-gif <treeses757@gmail.com>
2026-04-11 23:06:06 +0500

    Add files via upload

---
+++ src/js/games/UserGameController.js
@@ -0,0 +1,10 @@
+// ========== FILE: src/js/games/UserGameController.js ==========
+// Контроллер мультиплеера (упрощён)
+export class UserGameController {
+  constructor(rtdb, auth) {
+    this.rtdb = rtdb;
+    this.auth = auth;
+  }
+  
+  // Методы будут расширены в Matchmaker
+}
