27553d3c930cd69c68f52597f4184b197dcd2ce8
treeses757-gif <treeses757@gmail.com>
2026-04-11 12:08:00 +0500

    Update firebase-config.js

--- src/js/firebase-config.js
+++ src/js/firebase-config.js
@@ -2,6 +2,8 @@ export const firebaseConfig = {
   apiKey: "AIzaSyADQHyaiHrnCzk-IsrgZguP1Sl6eRqo9pc",
   authDomain: "minigames-fb308.firebaseapp.com",
   projectId: "minigames-fb308",
+  databaseURL: "https://minigames-fb308-default-rtdb.firebaseio.com", // ← вот эта строка
+  storageBucket: "minigames-fb308.firebasestorage.app",
   messagingSenderId: "73826070494",
   appId: "1:73826070494:web:23dd86d36861af4190f74f",
   measurementId: "G-TT3BD4T6FF"
